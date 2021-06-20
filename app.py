7c6acefc5c485d854c7bcefe9dff149cae0bae4d
Vladislav <oceanven@gmail.com>
2021-06-20 20:54:27 +0300

    first update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import requests 
+import requests
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -10,72 +10,56 @@ from lxml import html
 import re
 
 
-
-
 class ParseTeam:
-	def __init__(self, url):
-		self.url = url
-		r = requests.get(url)
-		self.tree = html.fromstring(r.content)
-		#soup = BeautifulSoup(r.text, 'html.parser')
+    def __init__(self, url):
+        self.url = url
+        r = requests.get(url)
+        self.tree = html.fromstring(r.content)
 
-		#options = webdriver.ChromeOptions()
-		#ptions.add_argument('--headless')
-		#self.driver = webdriver.Chrome(executable_path=driver_path, options=options)
-		#self.driver.get(self.url)
+    def parse_column(self, table_xpath='matchlogs_for', args=[]):
+        xpath_header_url = '//*[@id="{}"]/thead/tr//text()'.format(table_xpath)
 
-	def parse_column(self, table_xpath='matchlogs_for', args=[]):
-		xpath_header_url = '//*[@id="{}"]/thead/tr//text()'.format(table_xpath)		
+        l = self.tree.xpath('//*[@id="{0}"]/tbody/tr//text()'.format(table_xpath))
 
-		l = self.tree.xpath('//*[@id="{0}"]/tbody/tr//text()'.format(table_xpath))
-		
-	
+        # parse columns name
+        # header = re.findall(r' \w+ ',str(self.tree.xpath(xpath_header_url)))
+        re.findall(r'\w+', str(self.tree.xpath(xpath_header_url)))
+        list_header = re.findall(r'\w+', str(self.tree.xpath(xpath_header_url)))
+        print('lstheader:', list_header)
+        d = {}
+        count = 0
+        # create dict of columns name
+        for x in list_header:
+            d[x] = count
+            count += 1
 
-		# parse columns name
-		#header = re.findall(r' \w+ ',str(self.tree.xpath(xpath_header_url)))
-		re.findall(r'\w+',str(self.tree.xpath(xpath_header_url)))
-		list_header =  re.findall(r'\w+',str(self.tree.xpath(xpath_header_url)))
-		print('lstheader:',list_header)
-		d = {}
-		count = 0
-		# create dict of columns name
-		for x in list_header:
-			d[x] = count
-			count += 1
+        # output table by column name
+        # *args -- the list of name columns
+        json_team = {}
+        json_team['team'] = self.tree.xpath('//*[@id="meta"]/div[2]/h1/span[1]//text()')[0]
 
-		
+        for key, value in d.items():
+            json_team[key] = []
 
-		# output table by column name
-		# *args -- the list of name columns
-		json_team = {}
-		json_team['team'] = self.tree.xpath('//*[@id="meta"]/div[2]/h1/span[1]//text()')[0]
-		
-		for key, value in d.items():
-			json_team[key] = []
+            for arg in args:
+                json_team['id'] = 0
 
-			for arg in args:
-				json_team['id'] = 0
-				
+                if arg.lower() == key.lower():
 
-				if arg.lower() == key.lower():
+                    json_team['team'] = self.tree.xpath('//*[@id="meta"]/div[2]/h1/span[1]//text()')[0]
 
-					json_team['team'] = self.tree.xpath('//*[@id="meta"]/div[2]/h1/span[1]//text()')[0]
-					
+                    for column in range(1, len(l)):
+                        url = '//*[@id="{}"]/tbody/tr[{}]/td[{}]//text()'.format(table_xpath, column, value)
+                        u = self.tree.xpath(url)
+                        # print(u[0])
+                        try:
+                            json_team[key].append(u[0])
+                        except Exception:
+                            break
+                # self.tree.xpath(url))
 
-					
-					
-					for column in range(1, len(l)):
-						url = '//*[@id="{}"]/tbody/tr[{}]/td[{}]//text()'.format(table_xpath, column, value)
-						u = self.tree.xpath(url)
-						#print(u[0])
-						try:
-							json_team[key].append(u[0])
-						except Exception:
-							break
-						#self.tree.xpath(url))
-
-					#json_team['total_suma'] = lambda json_team['sum_column'] : json_team['column_count'] + len(l)
-		return json_team
+            # json_team['total_suma'] = lambda json_team['sum_column'] : json_team['column_count'] + len(l)
+        return json_team
 
 
 spain = ParseTeam('https://fbref.com/en/squads/b561dd30/Spain-Stats#matchlogs_for')
@@ -85,7 +69,7 @@ json_spain = spain.parse_column(args=['Result'])
 json_poland = poland.parse_column(args=['result', 'Venue', 'GF', 'Day'])
 
 pprint(json_poland)
-#pprint(json_spain)
+# pprint(json_spain)
 
 # //*[@id="matchlogs_for"]/tbody/tr[1]/td[6]
 # pprint(spain.parse_column())
